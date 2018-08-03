2ec7f75fab0c8c40eb23157a16e50939f8a9d0fb
Mateusz <mateusz.dworaczek@expereo.com>
2018-08-03 16:21:33 +0200

    nightwatch

--- src/app/containers/table/Table.js
+++ src/app/containers/table/Table.js
@@ -3,6 +3,7 @@ import ReactTable from "react-table";
 import "react-table/react-table.css";
 import { connect } from "react-redux";
 import { loadData } from "../../actions/index";
+import { Modal, Overlay } from "react-modal-construction-kit";
 
 const mapStateToProps = state => ({
   data: state.data,
@@ -17,33 +18,76 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class Table extends React.Component {
+  state = {
+    isModalVisible: false
+  };
+
   fetchData = (state, instance) => {
     this.props.load();
   };
 
+  showDetails = () => {
+    //orginal.name
+    this.setState({ isModalVisible: true });
+  };
+
+  close = () => {
+    this.setState({ isModalVisible: false });
+  };
+
   render() {
+    const { isModalVisible } = this.state;
+
     const columns = [
       {
         Header: "Name",
-        accessor: "name"
+        accessor: "name",
+        sortable: false
       },
       {
         Header: "Age",
         accessor: "age",
-        Cell: props => <span className="number">{props.value}</span>
+        Cell: props => <span className="number">{props.value}</span>,
+        sortable: false
+      },
+      {
+        id: "checkbox",
+        accessor: "",
+        Cell: ({ original }) => {
+          return (
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={false}
+              onChange={() => this.showDetails()}
+            />
+          );
+        },
+        Header: "Details",
+        sortable: false
       }
     ];
 
     return (
-      <ReactTable
-        data={this.props.data}
-        pages={this.props.pages}
-        loading={this.props.loading}
-        columns={columns}
-        manual
-        className="-striped -highlight"
-        onFetchData={this.fetchData}
-      />
+      <div>
+        <Modal
+          onClickOutside={this.close}
+          onClosed={this.close}
+          isOpen={isModalVisible}
+        >
+          <p>Here goes some details</p>
+        </Modal>
+        <Overlay isVisible={isModalVisible} />
+        <ReactTable
+          data={this.props.data}
+          pages={this.props.pages}
+          loading={this.props.loading}
+          columns={columns}
+          manual
+          className="-striped -highlight"
+          onFetchData={this.fetchData}
+        />
+      </div>
     );
   }
 }
